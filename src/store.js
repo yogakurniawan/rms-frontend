ec41b6421d3231264ac859dec4ffbddf7cfc6ed4
kholishulaziz <kholishul.aziz@gmail.com>
2017-05-02 11:01:26 +0700

    connect backend

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import createReducer from './reducers';
+import * as actionCreators from './action/actionCreators';
 
 export default function configureStore(initialState = {}) {
   const middlewares = [
@@ -25,6 +26,9 @@ export default function configureStore(initialState = {}) {
     initialState,
     composeEnhancers(...enhancers)
   );
+
+  // initial data
+  store.dispatch(actionCreators.requestGetEmployee());
 
   return store;
 }
